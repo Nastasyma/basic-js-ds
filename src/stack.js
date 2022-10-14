b555ddbb9e80b47f1189880ef59f8fe8748b40b3
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2022-10-14 22:08:46 +0300

    feat: add solution for stack.js

--- src/stack.js
+++ src/stack.js
@@ -13,22 +13,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
+  stack = [];
+  push(element) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    this.stack.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    return this.stack.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    return this.stack[this.stack.length-1]
   }
 }
 
 module.exports = {
   Stack
 };
+
+//const stack = new Stack();
+
+//stack.push(1) // adds the element to the stack
+//console.log(stack.peek()) // returns the peek, but doesn't delete it, returns 1
+//console.log(stack.pop()) // returns the top element from stack and deletes it, returns 1
+//console.log(stack.pop()) // undefined
